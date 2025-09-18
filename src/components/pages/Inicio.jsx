d56a30c0d38051301b11ace9e758e623029eece3
Cristian <diazcristian0210@gmail.com>
2025-09-18 16:03:55 -0300

    Commit pruebas

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -4,14 +4,13 @@ import CardProducto from "./producto/CardProducto";
 const Inicio = () => {
   return (
     <section className="mainSection">
-      
       <img
         className="banner"
         src="https://images.pexels.com/photos/28869109/pexels-photo-28869109.jpeg"
         alt="fondo cafe"
       />
       <Container className="mt-5">
-        <h1 className="display-4 text-underline-warning py-2">Nuestros Productos</h1>
+        <h1 className="display-4 text-underline-warning">Nuestros Productos</h1>
         <hr />
     
           <Row>
