65c9e2e78dfbcb2dde14c526c2dccdf7f20bb8b9
Cristian <diazcristian0210@gmail.com>
2025-09-26 12:07:48 -0300

    Agregue la logica de listar productos al inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,7 +1,25 @@
 import { Container, Row } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
+import { useEffect, useState } from "react";
+import { listarProductos } from "../helpers/queries";
 
 const Inicio = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    obtenerProductos();
+  }, []);
+
+  const obtenerProductos = async () => {
+    const respuesta = await listarProductos();
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      setProductos(datos);
+    } else {
+      alert("Error al listar los productos");
+    }
+  };
+
   return (
     <section className="mainSection">
       <img
@@ -12,15 +30,11 @@ const Inicio = () => {
       <Container className="mt-5">
         <h1 className="display-4 text-underline-warning">Nuestros Productos</h1>
         <hr />
-    
-          <Row>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-          </Row>
-       
+        <Row>
+          {productos.map((producto) => (
+            <CardProducto key={producto.id} producto={producto} />
+          ))}
+        </Row>
       </Container>
     </section>
   );
--- src/components/pages/producto/CardProducto.jsx
+++ src/components/pages/producto/CardProducto.jsx
@@ -1,18 +1,18 @@
 import { Col, Card, Button } from "react-bootstrap";
 import medialuna from "../../../assets/mljyq.webp"
-const CardProducto = () => {
+const CardProducto = ({producto}) => {
  
   return (
     <Col md={4} lg={3} className="mb-3">
       <Card className="h-100">
         <div>
-          <img src={medialuna} alt="cafe" className="card-img-top-nueva" />
+          <img src={producto.imagen} alt={producto.nombreProducto} className="card-img-top-nueva" />
         </div>
         <Card.Body>
-        <Card.Title className="primary-font">Capuchino</Card.Title>
+        <Card.Title className="primary-font">{producto.nombreProducto}</Card.Title>
         <Card.Text>
-          Descripción: Descripción: Espuma de leche cremosa sobre un espresso fuerte. <br className="mb-2"/> 
-          <span className="fw-bold">Precio: $350</span></Card.Text>
+          Descripción: {producto.descripcion} <br className="mb-2"/> 
+          <span className="fw-bold">Precio: {producto.precio}</span></Card.Text>
         </Card.Body>
         <Card.Footer className="text-end">
         <Button variant='warning' className="me-2" >Ver más</Button>
